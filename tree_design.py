5ed454bc90d578d1139071642ff1310c209c121b
Paula Eduarda de Lima. <125400664+PAULA-123@users.noreply.github.com>
2025-11-29 14:25:36 -0300

    Cria mecanismo inicial de state

--- tree_design.py
+++ tree_design.py
@@ -34,7 +34,6 @@ class Node(ABC):
 
 # nós internos (que fazem alguma decisão), herda de Node
 class DecisionNode(Node):
-
     def add_child(self, child: Node) -> None:
         self.children.append(child)
         print(f"DecisionNode: nó filho {child} adicionado")
@@ -70,6 +69,63 @@ class LeafNode(Node):
 ################################ STATE ##################################################
 
 # State: Classe TreeBuilder e estados como SplittingState, StoppingState e  PruningState. 
+
+
+class TreeBuilder():
+    """Contexto do padrão State. Classe que armazena uma referência a um dos objetos concretos de estado e delega a eles todos os trabalhos específicos de estado."""
+
+    _state = None # Define o estado atual da árvore
+
+    def __init__(self, State: State)-> None:
+        self.transition_to(State)
+
+    def transition_to(self, State: State): 
+        # permite alterar o objeto de estado em tempo de execução
+        print(f"TreeBuilder: Transition to {State}")
+        self._state = State
+        self._state.context = self
+
+    def build_step(self): 
+        # Método que delega o  comportamento para o estado
+        self._state.handle()
+
+
+
+
+
+class State(ABC):
+    """Classe abstrata que define o que todo State deve conter. Também fornece uma referência reversa à árvore do estado anterior"""
+    @property
+    def tree(self) -> TreeBuilder:
+        return self._tree
+
+    @property.setter
+    def tree(self, tree: TreeBuilder) -> None:
+        self._tree = tree
+
+    @abstractmethod
+    def handle(self) -> None:
+        pass
+
+
+class SplittingState(State):
+    """fase de divisão: cria decision nodes"""
+    
+    def handle(self)-> None:
+        print(f"SplittingState: Dividindo o nó... criando DecisionNode")
+        self.tree.transition_to(StoppingState())
+
+class StoppingState(State):
+    """fase de parada: cria leaf nodes"""
+    def  handle(self)-> None:
+        print("StoppingState: Parando divisão. Criando LeafNode.")
+        self.tree.transition_to(PruningState())
+
+class PruningState(State):
+    """fase de poda: remove ou reduz nós"""
+    def handle(self):
+        print("PruningState: Podando a árvore...")
+        self.tree.transition_to(SplittingState())
 
 
 
