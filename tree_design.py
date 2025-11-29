214ab9636ef73231b1de98fd8845e398d19858e4
Paula Eduarda de Lima. <125400664+PAULA-123@users.noreply.github.com>
2025-11-29 11:04:06 -0300

    Melhora estrutura do composite

--- tree_design.py
+++ tree_design.py
@@ -10,31 +10,61 @@ from typing import List
 
 class Node(ABC):
     """Classe abstrata que define o que todo nó deve possuir"""
+    def __init__(self):
+        self.children = []
+        self._is_leaf = False
 
-    # Não pode ser abstrato, pois os nós folhas não tem filhos
-    def add_child():
+    def is_leaf(self) -> bool:
+        return self._is_leaf
+
+    @abstractmethod
+    def add_child(self, child: Node) -> None:
         pass
     
     @abstractmethod
-    def accept(visitor):
+    def get_children(self)-> List[Node]:
         pass
 
-# nós internos (que fazem alguma decisão)
+    @abstractmethod
+    def accept(self, visitor)-> None:
+        pass
+
+ 
+
+
+# nós internos (que fazem alguma decisão), herda de Node
 class DecisionNode(Node):
-    def __init__(self):
-        self.chidren = []
 
-    def add_child():
-        pass
+    def add_child(self, child: Node) -> None:
+        self.children.append(child)
+        print(f"DecisionNode: nó filho {child} adicionado")
 
-    def accept(visitor):
-        return super().accept()
+    def get_children(self):
+        return self.children
+
+    def accept(self, visitor)-> None:
+        visitor.visit_decision_node(self)
+        print(f"DecisionNode: Visitor {visitor} aceito")
 
 
-# nós folha (resultado final)
+# nós folha (resultado final), herda de Node
 class LeafNode(Node):
-    def accept(visitor):
-        pass
+    def __init__(self):
+        super().__init__()
+        self._is_leaf = True
+
+
+    # Nó folha não pode ter filhos, então levanta um erro
+    def add_child(self, child: Node) -> None:
+        raise RuntimeError("LeafNode: Nós folhas não podem adicionar filhos!")
+    
+    def accept(self, visitor)-> None:
+        visitor.visit_leaf_node(self)
+        print(f"LeafNode: Visitor {visitor} aceito")
+
+    # Retorna lista vázia, já que não tem filhos
+    def get_children(self)-> List[Node]:
+        return []
 
 
 ################################ STATE ##################################################
